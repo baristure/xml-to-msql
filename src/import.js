a3d118797ec9cc77b8a6efdb295b8c1eb09f7fd2
Barış TÜRE <baristure@hotmail.com>
2020-07-02 00:39:17 +0300

    Update import.js

--- src/import.js
+++ src/import.js
@@ -1,10 +1,8 @@
 const logger = require('./logger');
 
-// const drBookCategoriesImport = require('./processors/drBookCategories');
 const xmlConvert = require('./processors/xmlconvert.js');
 
 module.exports = async (mysql) => {
     logger.info('New cycle has started');
-    // await drBookCategoriesImport(mysql);
     await xmlConvert(mysql);
 };
